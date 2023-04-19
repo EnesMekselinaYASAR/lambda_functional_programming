a5570b803cb5bedee613a49115940e72fe9774c4
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-19 14:08:08 +0300

    19.04.2023

--- src/lambda_functional_programming/Fp01.java
+++ src/lambda_functional_programming/Fp01.java
@@ -30,24 +30,101 @@ public class Fp01 {
         liste.add(8);
         System.out.println(liste); // [8, 9, 131, 10, 9, 10, 2, 8]
         listElamanlariniYazdirStructure(liste);
+        System.out.println("");
+        listElamanlariniYazdirFunctional(liste);
+        System.out.println("");
+        ciftElemanlariYazdirStructure(liste);
+        System.out.println("");
+        ciftElemanlariYazdirFunctional(liste);
+        System.out.println("");
+        tekElemanlarinKareleriniYazdir(liste);
+        System.out.println("");
+        tekrarsizTekElemanlarinKupunuYazdir(liste);
+        System.out.println("");
+        tekrarsizCiftElemanlarinKareToplami(liste);
 
     }
-
         /*
         1- ) Ardışık list elementlerini aynı satırda aralarında boşluk bırakarak
         yazdıran bir method oluşturun.(Structured)
          */
 
-    public static void listElamanlariniYazdirStructure(List<Integer> list){
+    public static void listElamanlariniYazdirStructure(List<Integer> list) {
 
-        for (Integer w : list){
+        for (Integer w : list) {
 
-            System.out.print(w+" "); // 8 9 131 10 9 10 2 8
+            System.out.print(w + " "); // 8 9 131 10 9 10 2 8
 
         }
-
-
     }
+
+    /*
+    1- ) Ardışık list elementlerini aynı satırda aralarında boşluk bırakarak
+    yazdıran bir method oluşturun.(Functional)
+     */
+    public static void listElamanlariniYazdirFunctional(List<Integer> list) {
+        list.stream().forEach(t -> System.out.print(t + " ")); // 8 9 131 10 9 10 2 8
+
+            /*
+        --> stream() method collection'dan elementleri akışa dahil etmek için ve
+            methodlara ulaşmak için kullanilir.
+         */
+    }
+
+    /*
+    2- )Cift list elemanlarini aynı satırda aralarında boşluk bırakarak
+    yazdıran bir method oluşturun.(Structured)
+    */
+    public static void ciftElemanlariYazdirStructure(List<Integer> list) {
+        for (Integer w : list) {
+            if (w % 2 == 0) {
+                System.out.print(w + " "); // 8 10 10 2 8
+            }
+        }
+    }
+
+    /*
+    2- )Cift list elemanlarini aynı satırda aralarında boşluk bırakarak
+    yazdıran bir method oluşturun.(Funcional)
+    */
+    public static void ciftElemanlariYazdirFunctional(List<Integer> list) {
+        list.stream().filter(t -> t % 2 == 0).forEach(t -> System.out.print(t + " ")); // 8 10 10 2 8
+    }
+    /*
+    3- ) Ardışık tek list elementlerinin karelerini aynı
+    satırda aralarında boşluk bırakarak yazdıran bir method oluşturun.(Functional)
+    */
+
+    public static void tekElemanlarinKareleriniYazdir(List<Integer> list){
+        list.stream().filter(t->t%2!=0).map(t->t*t).forEach(t-> System.out.print(t+" ")); // 81 17161 81
+        // elemanlarin degerleri degisecekse map() methodu kullanilir.
+    }
+
+    /*
+    4- ) Ardışık tek list elementlerinin küplerini tekrarsız olarak aynı
+    satırda aralarında boşluk bırakarak yazdıran bir method oluşturun.
+     */
+
+    public static void tekrarsizTekElemanlarinKupunuYazdir(List<Integer>list){
+        list.stream().distinct().filter(t->t%2!=0).map(t->t*t*t).forEach(t-> System.out.print(t+" ")); // 729 2248091
+    }
+
+     /*
+    5- ) Tekrarsız çift elementlerin karelerinin toplamını hesaplayan bir method
+    oluşturun.
+     */
+
+    public static void tekrarsizCiftElemanlarinKareToplami(List<Integer>list){
+        Integer toplam=list.stream().distinct().filter(t->t%2==0).map(t->t*t).reduce(0,(t,u)->t+u);
+        System.out.println(toplam); // 168
+    }
+
+     /*
+    6- ) Tekrarsız çift elemanların küpünün çarpımını hesaplayan bir method oluşturun.
+     */
+
+    public static
+
 
 
 }
