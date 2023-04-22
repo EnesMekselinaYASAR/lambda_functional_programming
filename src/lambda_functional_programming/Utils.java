79ba569abba6c277e39506a68c46966fea8988b9
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-22 23:38:42 +0300

    22.04.2023

--- src/lambda_functional_programming/Utils.java
+++ src/lambda_functional_programming/Utils.java
@@ -2,22 +2,27 @@ package lambda_functional_programming;
 
 public class Utils {
     public static void aynıSatirdaBosluklaYAzdir(Object obj){
+
         System.out.print(obj+" ");
     }
 
     public static boolean ciftElemanlariSec(int x){
+
         return x%2==0;
     }
 
     public static boolean tekElemanlariSec(int x){
+
         return x%2!=0;
     }
 
     public static int karesiniAl(int x){
+
         return x*x;
     }
 
     public static int kupunuAl(int x){
+
         return x*x*x;
     }
 
