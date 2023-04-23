5ef2651c22befeab3aa8d1a84464e2259451346a
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-23 10:17:29 +0300

    23.04.2023

--- src/lambda_functional_programming/Fp03.java
+++ src/lambda_functional_programming/Fp03.java
@@ -1,5 +1,6 @@
 package lambda_functional_programming;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -17,24 +18,39 @@ public class Fp03 {
         liste.add("Tucker");
         liste.add("Benjamin");
         System.out.println(liste);
-        buyukHarfleYazdir(liste); // ALİ ALİ MARK AMANDA CHRİSTOPHER JACKSON MARİANO ALBERTO TUCKER BENJAMİN
+        buyukHarfleYazdir01(liste); // ALİ ALİ MARK AMANDA CHRİSTOPHER JACKSON MARİANO ALBERTO TUCKER BENJAMİN
         System.out.println();
-        buyukHarfleYazdir02(liste); //
+        uzunlugaGoreYazdir(liste); // Ali Ali Mark Amanda Tucker Jackson Mariano Alberto Benjamin Christopher
+
+
     }
 
     /*
     1- ) Tüm elemanları büyük harf ile yazdıran bir method oluşturun.
      */
     // 1.yol;
-    public static void buyukHarfleYazdir(List<String>list){
+    public static void buyukHarfleYazdir01(List<String>list){
         list.stream().map(String::toUpperCase).forEach(Utils::aynıSatirdaBosluklaYAzdir);
     }
 
-    // 2.yol;
-    // public static void buyukHarfleYazdir02(List<String>list){
-    //     list.replaceAll(String::toUpperCase);
-    //     System.out.print(list);
-    // }
+     /*
+     // 2.yol;
+     public static void buyukHarfleYazdir02(List<String>list){
+         list.replaceAll(String::toUpperCase);
+         System.out.print(list);
+     }
+    Listemiz mutable oldugu icin yorum satirina aldik. Bu method listemizi degistirdi.
+    */
+
+    /*
+   2- ) Elemanları uzunluklarına göre sıralayıp yazdıran bir method oluşturun.
+    */
+    public static void uzunlugaGoreYazdir(List<String> list){
+
+        list.stream().sorted(Comparator.comparing(String::length)).forEach(Utils::aynıSatirdaBosluklaYAzdir);
+        // Comparator.comparing(); --> siralama kosulunu belirtmek icin kullanilir
+
+    }
 
 
 }
